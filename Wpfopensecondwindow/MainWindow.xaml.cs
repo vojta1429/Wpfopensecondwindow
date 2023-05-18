b493e84b65fed5aa8b554098ee26b70e52c7afe2
vojta1429 <95287318+vojta1429@users.noreply.github.com>
2023-05-18 12:35:13 +0200

    Přidejte soubory projektu.

---
+++ Wpfopensecondwindow/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpfopensecondwindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenWindow(object sender, RoutedEventArgs e)
+
+
+        {
+            Window1 objWindow1 = new Window1();
+            this.Visibility = Visibility.Hidden;
+            objWindow1.Show();
+        }
+
+        private void OpenWindow2(object sender, RoutedEventArgs e)
+        {
+            Window2 objWindow2 = new Window2();
+            this.Visibility = Visibility.Hidden;
+            objWindow2.Show();
+
+        }
+    }
+}
---
+++ Wpfopensecondwindow/Window2.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace Wpfopensecondwindow
+{
+    
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+            DataContext = new Window2();
+        }
+
+        private void ButtonAddName_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text) || lstNames.Items.Contains(txtName.Text))
+            {
+                return;
+            }
+            lstNames.Items.Add(txtName.Text);
+            txtName.Clear();
+        }
+
+    }
+}
